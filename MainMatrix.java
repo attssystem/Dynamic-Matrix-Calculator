4d92016c702e13babb8e23b66a14e3b117df49d1
ATTSSystem <atts.studio@gmail.com>
2019-05-19 11:17:49 +0200

    Main menu update
    Split SHOW and SHOW_ALL functions
    |_>Update showMatrix and showMatrices in consequence

--- MainMatrix.java
+++ MainMatrix.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class MainMatrix {
 	
@@ -13,15 +12,15 @@ public class MainMatrix {
         
         // Tests
         
-        /*Matrix m = new Matrix(3,3, "TEST");
+        Matrix m = new Matrix(3,3, "test");
         matricesTab[0] = m;
-		showMatrix(0, matricesTab,true);
-		dynamicEditor("TEST", matricesTab, sc);*/
+		showMatrix(0, matricesTab,true, true, sc);
+		//dynamicEditor("TEST", matricesTab, sc);
 		
 		// Starting UI
 		
 		clearScreen();
-		info = "To begin, you can create your first Matrix using the 'CREATE' command\nand by letting the program guide you through the creation process,\nthen you can use 'EDIT', 'CALC', 'CREATE' again or 'QUIT'.";
+		info = "To begin, you can create your first Matrix using the 'CREATE' command\nand by letting the program guide you through the creation process,\nthen you can use 'EDIT', 'CALC', 'CREATE', 'SHOW', 'SHOW_ALL' again or 'QUIT'.";
 		System.out.println();
         
         while(!quit) {
@@ -31,11 +30,22 @@ public class MainMatrix {
 			Scanner mainWhile_sc = new Scanner(cmd);
 			clearScreen();
 			if(mainWhile_sc.hasNext("EDIT") || mainWhile_sc.hasNext("edit")){
-				//
+				System.out.print("Matrix Name ? >");
+				cmd = sc.next();
+				if(!dynamicEditor(cmd, matricesTab, sc, info)){
+					info = "Error while searching matrix";
+				}
 			}else if(mainWhile_sc.hasNext("CALC") || mainWhile_sc.hasNext("calc")){
 				//doOperation();
 			}else if(mainWhile_sc.hasNext("CREATE") || mainWhile_sc.hasNext("create")){
 				//CreateMatrix();
+			}else if(mainWhile_sc.hasNext("SHOW_ALL") || mainWhile_sc.hasNext("show_all")){
+				showMatrices(matricesTab, sc);
+			}
+			else if(mainWhile_sc.hasNext("SHOW") || mainWhile_sc.hasNext("show")){
+				System.out.print("Matrix Name ? >");
+				cmd = sc.next();
+				showMatrix(searchMatrix(cmd, matricesTab), matricesTab, false, true, sc);
 			}else if(mainWhile_sc.hasNext("QUIT") || mainWhile_sc.hasNext("quit")){
 				quit = true;
 			}else{
@@ -50,34 +60,40 @@ public class MainMatrix {
   
 	// Show all matrices
 	
-    public static void showMatrices(Matrix[] matricesTab) {
+    public static void showMatrices(Matrix[] matricesTab, Scanner sc) {
         for(int i = 0; i < matricesTab.length; i++) {
-            showMatrix(i, matricesTab, false);
+            showMatrix(i, matricesTab, false, false, sc);
             System.out.println();
         }
+        pause(sc);
     }
     
     // Show one identified matrix
     
-    public static void showMatrix(int id, Matrix[] matricesTab, boolean b) {
+    public static void showMatrix(int id, Matrix[] matricesTab, boolean b, boolean one, Scanner sc) {
         System.out.println("| Matrix name :"+matricesTab[id].name);
         System.out.println();
         System.out.print("    ");
-        for(int i = 0; i < matricesTab[id].data.length; i++) {
-			System.out.print("  "+(i+1)+" ");
+        if(b){
+			for(int i = 0; i < matricesTab[id].data.length; i++) {
+				System.out.print("  "+(i+1)+" ");
+			}
 		}
 		System.out.println();
         for(int i = 0; i < matricesTab[id].data.length; i++) {
 			if(b) {
 				System.out.print(" "+(i+1)+" ");
 			}
-			System.out.print("[");
+			System.out.print("|");
             for(int j = 0; j < matricesTab[id].data.length; j++) {
 				System.out.print(" "+matricesTab[id].data[i][j]+" ");
 			}
-			System.out.println("]");
+			System.out.println("|");
 		}
 		System.out.println();
+		if(one){
+			pause(sc);
+		}
     }
     
     // Look for a specific matrix using his name
@@ -85,10 +101,13 @@ public class MainMatrix {
     public static int searchMatrix(String name, Matrix[] matricesTab) {
         int id = -1;
         int i = 0;
-        while(id == -1) {
-            if(matricesTab[i].name == name) {
-                id = i;
-            }
+        while(id == -1 && i < matricesTab.length) {
+			System.out.println(i);
+			System.out.println(matricesTab[i].name);
+			System.out.println(name);
+			if(matricesTab[i].name != name){                      // A MODIFIER CAR CE N'EST PAS NORMAL !!!!!!!!!
+				id = i;
+			}
             i++;
         }
         return id;
@@ -107,42 +126,55 @@ public class MainMatrix {
 	
 	// Dynamic Editor
 	
-	public static void dynamicEditor(String name, Matrix[] matricesTab, Scanner sc) {
-		String com = "Type x,y,new_value or 0 to leave";
-		boolean quit = false;
-		String input;
-		int a;
-		int b;
-		int c;
-		
-		int id = searchMatrix("TEST", matricesTab);
-		while(!quit) {
-			clearScreen();
-			System.out.println("------------------");
-			System.out.println("| DYNAMIC EDITOR |");
-			System.out.println("------------------");
-			System.out.println("| Info :"+com);
-			showMatrix(searchMatrix("TEST", matricesTab), matricesTab, true);
-			System.out.print("DynEdit_CMD> ");
-			input = sc.next();
-			// Write text will stop program !
-			Scanner dynEdit_sc = new Scanner(input);
-			dynEdit_sc.useDelimiter(",");
-			a = dynEdit_sc.nextInt();
-			if(a == 0) {
-				quit = true;
-			}else{
-				b = dynEdit_sc.nextInt();
-				c = dynEdit_sc.nextInt();
-				dynEdit_sc.close();
-				if(a <= matricesTab[id].data.length && 0 < a && b <= matricesTab[id].data[a].length && 0 < b) {
-					matricesTab[id].data[a-1][b-1] = c;
-					com = "Change done !";
-				}else {
-					com = "Wrong input, retry";
+	public static boolean dynamicEditor(String name, Matrix[] matricesTab, Scanner sc, String info) {
+		int id = searchMatrix(name, matricesTab);
+		System.out.println(id);
+		if(id == -1){
+			return false;
+		}else{
+			String com = "Type x,y,new_value or 0 to leave";
+			boolean quit = false;
+			String input;
+			int a;
+			int b;
+			int c;
+			while(!quit) {
+				clearScreen();
+				System.out.println("------------------");
+				System.out.println("| DYNAMIC EDITOR |");
+				System.out.println("------------------");
+				System.out.println("| Info :"+com);
+				showMatrix(id, matricesTab, true, false, sc);
+				System.out.print("DynEdit_CMD> ");
+				input = sc.next();
+				// Write text will stop program !
+				Scanner dynEdit_sc = new Scanner(input);
+				dynEdit_sc.useDelimiter(",");
+				a = dynEdit_sc.nextInt();
+				if(a == 0) {
+					quit = true;
+					info = "To begin, you can create your first Matrix using the 'CREATE' command\nand by letting the program guide you through the creation process,\nthen you can use 'EDIT', 'CALC', 'CREATE' again, 'SHOW', 'SHOW_ALL' or 'QUIT'.";
+				}else{
+					b = dynEdit_sc.nextInt();
+					c = dynEdit_sc.nextInt();
+					dynEdit_sc.close();
+					if(a <= matricesTab[id].data.length && 0 < a && b <= matricesTab[id].data[a].length && 0 < b) {
+						matricesTab[id].data[a-1][b-1] = c;
+						com = "Change done !";
+					}else {
+						com = "Wrong input, retry";
+					}
 				}
 			}
+			return true;
 		}
+	}
+	
+	// Block program
+	
+	public static void pause(Scanner sc) {
+		System.out.println("Type anything to go back to menu");
+        sc.next();
 	}
 	
 	// Clear the screen
