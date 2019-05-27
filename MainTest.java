056956120edf9a5765a7bb40e5c4f8b76d2230a7
AstraleK <AstraleK@users.noreply.github.com>
2019-05-27 11:23:23 +0200

    Add multiply
    - multiply by scalar
    - multiply by matrix

---
+++ MainTest.java
@@ -0,0 +1,13 @@
+public class MainTest {
+    public static void main(String[] args) {
+		 
+		 Matrix m01 = new Matrix(3,2,"truc",true);
+		 m01.afficheMatrice();
+		 
+		 Matrix m02 = new Matrix(
+		 m02.afficheMatrice();
+		 
+		 Matrix m03 = m01.multiplyM(m02);
+		 m03.afficheMatrice();
+	}
+}
